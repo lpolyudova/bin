23768c275c319aeb76d57325da0f087671b55587
lpolyudova <lpolyudova@soasta.com>
2015-10-12 19:36:36 -0700

    progress on junit writer

--- junit/junit_write.py
+++ junit/junit_write.py
@@ -19,8 +19,22 @@ read line -> g
 
 
 create_stack = list()
-allowed_commands = { "create", "delete", "move", "copy", "rename", "done"}
+allowed_commands = { "create", "delete", "move", "copy", "rename", "done",\
+                     "vcscheckout", "vcsadd", "vcsrevert",\
+                     "get"}
+ID_to_obj = {}
 
+def get_prologue(type):
+    prologue = ""
+    if type=="vcsrepository":
+        prologue="String oGitFolderPath = getGitRepoPath();\n"
+    return prologue
+
+def get_args(test_object_a):
+    args=""
+    if test_object_a.o_type.lower()=="vcsrepository":
+        args = "oGitFolderPath";
+    return args
 
 def capitalize(string):
     return string[0].upper()+string[1:].lower()
@@ -28,13 +42,13 @@ def capitalize(string):
 
 def generate_stub(target, create_flag):
     Name = target.name
-    Type = target.o_type.capitalize()
-    prologue=""
-    args=""
+    Type = target.o_type
+    prologue=get_prologue(Type.lower())
+    args=get_args(target)
     options=""
     for attr in target.attributes.keys():
         value = target.attributes[attr]
-        value_obj = test_object_map[value];
+        value_obj = ID_to_obj[value];
         options = "o"+Name+".set"+attr+"("+value_obj.ID+");\n"
     create_stub=prologue+\
     "Object o"+Name+" = getGeneric"+Type+"("+args+");\n"+\
@@ -42,7 +56,7 @@ def generate_stub(target, create_flag):
     "long l"+Name+"ID = m_oPersistenceSession.createObject(o"+Name+");\n"+\
     "try\n {"
     delete_stub="}\n finally\n {"+\
-    "m_oPersistenceSession.deleteObjectByID("+Type.lower()+", l"+Name+"ID);\n"+\
+    "m_oPersistenceSession.deleteObjectByID(\""+Type.lower()+"\", l"+Name+"ID);\n"+\
     "}"
     if create_flag=="create":
         return create_stub
@@ -52,18 +66,17 @@ def generate_stub(target, create_flag):
 test_object_map = {}
 class test_object(object):
     def __init__(self,args):
-        print args
-        self.attributes = list()
+        self.attributes = {}
         [o_type, quant] = args[0].split('=')
         args = args[1:]
-        self.name = o_type.capitalize()+quant
-        self.o_type=o_type.lower()
+        self.name = o_type+quant
+        self.o_type=o_type
         self.ID = "l"+self.name+"ID";
         for arg in args:
             [attr, value] = arg.split('=')
             self.attributes[attr] = value
         test_object_map[self.name]=self
-
+        ID_to_obj[self.ID]=self
     
 class command(object):
     def __init__(self,args):
@@ -89,8 +102,8 @@ def done_cmd(args):
 def move_cmd(args):
     target = test_object_map[args[0]]
     new_location = test_object_map[args[1]]
-    target.attribtues['ParentID'] = new_location.ID
-    print "m_oPersistenceSession.moveObjectByID(\""+target.o_type+\
+    target.attributes['ParentID'] = new_location.ID
+    print "m_oPersistenceSession.moveObjectByID(\""+target.o_type.lower()+\
     "\", "+target.ID+", "+new_location.ID+");\n"
 
 def copy_cmd(args):
@@ -100,19 +113,59 @@ def copy_cmd(args):
     new_name.attributes = target.attributes;
     new_name.attributes['ParentID'] = new_location.ID
     create_stack.append(new_name);
-    print "long "+new_name.ID+" = m_oPersistenceSession.copyObjectByID(\""+target.o_type+\
+    print "long "+new_name.ID+" = m_oPersistenceSession.copyObjectByID(\""+target.o_type.lower()+\
     "\", "+target.ID+", "+new_location.ID+", \""+target.name+"Copy\");\n"+\
     "try\n{"
     
 def delete_cmd(args):
     target = test_object_map[ags[0]]
-    print "m_oPersistenceSession.deleteObjectByID(\""+target.o_type+\
+    print "m_oPersistenceSession.deleteObjectByID(\""+target.o_type.lower()+\
         "\", "+target.ID+");\n"
 
+def vcscheckout_cmd(args):
+    folder = test_object_map[args[0]]
+    vcsrepo = test_object_map[args[1]]
+    print " m_oPersistenceSession.vcsCheckOut("+folder.ID +\
+        ", "+ vcsrepo.ID+");\n"
+    test_object(['Composition=1', 'ParentID='+folder.ID, 'jName="SoastaComp"'])
+    test_object(["Target=1", "ParentID="+folder.ID, 'jName="soasta"'])
+    test_object(["MessageClip=1", "ParentID="+folder.ID, 'jName="Clip for Soasta"'])
+
+def vcsadd_cmd(args):
+    Id_array = "new long[] {"+(", ").join([test_object_map[item].ID for item in args])+"}"
+    print "m_oPersistenceSession.vcsAdd("+Id_array+");\n"
+    
+
+def vcsrevert_cmd(args):
+    Id_array = "new long[] {"+(", ").join([test_object_map[item].ID for item in args])+"}"
+    "m_oPersistenceSession.vcsRevert("+Id_array+");\n"
+
+def get_cmd(args):
+    [target, new_name] = args[0].split('=')
+    target = test_object_map[target]
+    [find_flag, identifier] = args[1].split('=')
+    if new_name=="":
+        isNew = ""
+    else:
+        test_object_map[new_name]=target
+        isNew="Object "
+
+    if find_flag.lower()=="id":
+        method="getObjectByID("
+        args="\""+target.o_type.lower()+"\", "+target.ID+");\n"
+    elif find_flag.lower()=="path":
+        path="str"+identifier+"Path"
+        print "String "+path+" = Folders.mergePathAndBaseName(o"+identifier+".getPath(), o"+identifier+".getName());"
+        method="getObjectByName("
+        args="\""+target.o_type.lower()+"\", "+path+", "+target.attributes['jName']+");\n"
+    print isNew+"o"+target.name+" = m_oPersistenceSession."+method+args;
+    
+# main routine is here
 with open('command_file') as f:
     for line in f:
         line = line.rstrip()
-        command(line.split(' '))
+        if not line[0]=="#":
+            command(line.split(' '))
         
 
     
